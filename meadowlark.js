fd10c6d90844d467164b8242c6f4c5a90d2fde0d
maradeben <maradebenezer@gmail.com>
2024-10-24 09:48:11 -0700

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
-var express = require('express')
+var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -9,31 +10,32 @@ var handlebars = require('express3-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));
+
+
+
+
 // home page
 app.get('/', function (req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 // about page
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
 app.use(function(req, res, next){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
     console.error(err.stack)
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
